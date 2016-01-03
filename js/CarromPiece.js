55f171874ccebff5e947e30cbde6c82f43d67538
santosh <santospandey333@gmail.com>
2016-01-03 20:10:32 +0545

    sound added

--- js/CarromPiece.js
+++ js/CarromPiece.js
@@ -3,6 +3,8 @@ function CarromPiece()
 	var that = this;	
 	this.element = document.createElement("div");
     
+    this.queen = false;
+    this.white;
     this.mass;
     this.velocity = 10;
     that._velocity; 
@@ -14,11 +16,11 @@ function CarromPiece()
     this.radius = 10;    
     this.distanceTravelled = 0;
     this.times = 0;
-    this.boardHeight = 600;
     this.boardWidth = 600;
     this.boardBorder = 52;
     this.getVelo = 0;
     this.holeRadius = 16;
+    var sound = new Audio("C:/Users/Santosh/Desktop/crm-backup/carrom/sound/tictic.wav");    
 
 	this.appendTo = function(parentElement) 
 	{
@@ -30,7 +32,7 @@ function CarromPiece()
         var left = posLeft - that.radius;
         var top = posTop - that.radius;
 	    that.element.style.left = left + 'px';
-	    that.element.style.top = top + 'px';
+	    that.element.style.top = top + 'px';        
 		that.x = parseInt(window.getComputedStyle(that.element).getPropertyValue('left')); 
 	    that.y = parseInt(window.getComputedStyle(that.element).getPropertyValue('top'));	       	    
 	}
@@ -59,7 +61,7 @@ function CarromPiece()
 	    that.x += that.dx;
 	    that.y += that.dy; 
 
-        if(Math.abs(that.dx) <= 0.2 && Math.abs(that.dy) <= 0.2)
+        if(Math.abs(that.dx) <= 0.4 && Math.abs(that.dy) <= 0.4)
         {
             that.dx = 0;
             that.dy = 0;
@@ -70,48 +72,68 @@ function CarromPiece()
                 
 	    that.element.style.left = that.x + 'px';
 	    that.element.style.top = that.y + 'px';   
+    }
 
+    this.checkhole = function()
+    {
         // condition to check if gotti will be inside the hole
-        if((that.x<(that.boardBorder+that.holeRadius)) && (that.y < (that.boardBorder+that.holeRadius)))
-        {            
-            that.element.style.background = 'transparent';
+        if((that.x<(that.boardBorder+that.holeRadius)) && 
+            (that.y < (that.boardBorder+that.holeRadius)))
+        { 
+            return true;
+            // console.log(that.x + "  " + that.y);
+            // that.element.style.background = 'transparent';
         }
 
-        if(that.x < (that.boardBorder+that.holeRadius) && that.y > (that.boardWidth - that.boardBorder - that.holeRadius))
-        {            
-            that.element.style.background = 'transparent';
+        if(that.x<(that.boardBorder+that.holeRadius) && 
+            that.y > (that.boardWidth - that.boardBorder - that.holeRadius - 1.5*that.radius))
+        {
+            return true; 
+            // console.log(that.x + "  " + that.y);
+            // that.element.style.background = 'transparent';
         }
 
-        if(that.x > (that.boardWidth - that.boardBorder - that.holeRadius) && that.y < (that.boardBorder+that.holeRadius))
-        {            
-            that.element.style.background = 'transparent';
+        if(that.x > (that.boardWidth - that.boardBorder - that.holeRadius - 1.5*that.radius) && 
+            that.y < (that.boardBorder+that.holeRadius))
+        {
+            return true;            
+            // that.element.style.background = 'transparent';
         }
 
-        if(that.x > (that.boardWidth - that.boardBorder - that.holeRadius) && that.y > (that.boardWidth - that.boardBorder - that.holeRadius))
-        {            
-            that.element.style.background = 'transparent';
+        if(that.x > (that.boardWidth - that.boardBorder - that.holeRadius - 1.5*that.radius) && 
+            that.y > (that.boardWidth - that.boardBorder - that.holeRadius - 1.5*that.radius))
+        {
+            return true;            
+            // that.element.style.background = 'transparent';
         }
+
+        else
+            return false;
     }
 
     this.detectWall = function()
-    {    	
+    {
     	if(that.x>(that.boardWidth - that.boardBorder - 2*that.radius))
     	{   
-    		that.dx = -Math.abs(that.dx);    		
+    		that.dx = -Math.abs(that.dx);   
+            sound.play();            
     	}        
         else if( that.x < (that.boardBorder))
         {
             that.dx = Math.abs(that.dx);
+            sound.play();            
         }
 
-    	if(that.y>(that.boardHeight - that.boardBorder - 2*that.radius)) 
+    	if(that.y>(that.boardWidth - that.boardBorder - 2*that.radius)) 
     	{    	
     		that.dy = -Math.abs(that.dy);
+            sound.play();                        
     	}
         else if(that.y < that.boardBorder)
         {
             that.dy = Math.abs(that.dy);
-        }    	
+            sound.play();                        
+        }            
     }
 
     this.hitTest = function(otherCarrom)
@@ -119,6 +141,7 @@ function CarromPiece()
         if(Math.sqrt((that.x - otherCarrom.x)*(that.x - otherCarrom.x) + 
             (that.y - otherCarrom.y)*(that.y - otherCarrom.y)) < (that.radius + otherCarrom.radius))
         {
+            sound.play();
             return true;
         }
         else 
@@ -132,19 +155,12 @@ function CarromPiece()
         // for unit vector calculation
         var v2X = otherCarrom.x - that.x;
         var v2Y = otherCarrom.y - that.y;
-        var distance = Math.sqrt(Math.pow((otherCarrom.x - that.x),2) + Math.pow((otherCarrom.y - that.y), 2));
+        var distance = Math.sqrt(Math.pow((otherCarrom.x - that.x),2) + Math.pow((otherCarrom.y - that.y), 2));        
 
         if(distance == 0)
         {
             distance = 1;
         }
-
-        // // for reducing divide by zero problem
-        // if(v2Y == 0 && v2Y == 0)
-        // {
-        //     v2Y = 1;
-        //     v2X = 1;
-        // }
 
         otherCarrom.dx = (that.velocity*v2X/(distance))/otherCarrom.mass;
         otherCarrom.dy = (that.velocity*v2Y/(distance))/otherCarrom.mass;                
@@ -157,21 +173,33 @@ function CarromPiece()
         // checking the boundry of the hole   
         if(that.x<70)
         {
-            that.x += 7;                
+            that.x += 1;                
         }        
-            that.x -= 7;
-            that.moveGotti();                            
+            that.x -= 1;
+            that.moveGotti();   
     }
 
     this.slideRight = function()
     {
-         if(that.x > 490)
-         {
-            that.x -= 7;    
-         }
+        if(that.x > 490)
+        {
+           that.x -= 1;    
+        }
 
-         that.x += 7; 
-         that.moveGotti();       
+        that.x += 1; 
+        that.moveGotti();       
+    }
+
+    this.slideup = function()
+    {
+        that.y -= 1;
+        that.moveGotti();
+    }
+
+    this.slidedown = function()
+    {
+        that.y += 1;
+        that.moveGotti();
     }
 
     this.eventHandling = function(event)
@@ -235,5 +263,15 @@ function CarromPiece()
                 that.initGottiPos(280, 468);
             }
         }
+
+        // if(event.keyCode == 69)
+        // {
+        //     that.slideup();            
+        // }
+
+        // if(event.keyCode == 70)
+        // {
+        //     that.slidedown();            
+        // }
     }        
 }
--- js/CarromPieceMove.js
+++ js/CarromPieceMove.js
@@ -2,7 +2,9 @@ function CarromPieceMove()
 {
     var that = this;
 	var striker;
-	var totalCarromMen;	
+	var totalCarromMen;
+    var totalwhite;
+    var totalblack;	
 	var carromMen;
     var wrapper;
     var carromBoard;
@@ -11,7 +13,9 @@ function CarromPieceMove()
 // initialize variables of class.
     this.initialization = function()
     {
-        totalCarromMen = 10;
+        totalCarromMen= 19;
+        totalblack = 9;
+        totalwhite = 9;
         carromMen = [];
 
         wrapper = document.createElement("div");
@@ -56,30 +60,72 @@ function CarromPieceMove()
 		    carromMen[i].appendTo(carromBoard);	
             carromMen[i].mass = 1;	      		    		
 		}
+            carromMen[0].initGottiPos(240,300);
+            carromMen[0].white = true;
+            carromMen[1].initGottiPos(260,300); 
+            carromMen[1].white = true;
+            carromMen[2].initGottiPos(280,300);
+            carromMen[2].white = true;
+            carromMen[3].initGottiPos(260,280);   
+            carromMen[3].white = true;         
+            carromMen[4].initGottiPos(320,300);
+            carromMen[4].white = true;
+            carromMen[5].initGottiPos(340,300);
+            carromMen[5].white = true;
+            carromMen[6].initGottiPos(360,300);
+            carromMen[6].white = true;
+            carromMen[7].initGottiPos(240,320);
+            carromMen[7].white = true;
+            carromMen[8].initGottiPos(260,320); 
+            carromMen[8].white = true;
 
-            carromMen[0].initGottiPos(260,250);        
-            carromMen[1].initGottiPos(340,250);
-            carromMen[2].initGottiPos(200,250);
-            carromMen[3].initGottiPos(160,300);
-            carromMen[4].initGottiPos(220,300);
-            carromMen[5].initGottiPos(280,300);
-            carromMen[6].initGottiPos(160,350);
-            carromMen[7].initGottiPos(220,350);
-            carromMen[8].initGottiPos(280,350);
-            carromMen[9].initGottiPos(340,345);    
+            carromMen[9].initGottiPos(280,320);
+            carromMen[9].white = false;
+            carromMen[10].initGottiPos(300,320);
+            carromMen[10].white = false;
+            carromMen[11].initGottiPos(320,320);
+            carromMen[11].white = false;
+            carromMen[12].initGottiPos(340,320);
+            carromMen[12].white = false;
+            carromMen[13].initGottiPos(360,320);
+            carromMen[13].white = false;
+            carromMen[14].initGottiPos(280,280);
+            carromMen[14].white = false;
+            carromMen[15].initGottiPos(300,280);
+            carromMen[15].white = false;
+            carromMen[16].initGottiPos(320,280); 
+            carromMen[16].white = false;
+            carromMen[17].initGottiPos(340,280);
+            carromMen[17].white = false;
+
+            carromMen[18].initGottiPos(300,300);
+            carromMen[18].element.style.backgroundColor = "red";
+            carromMen[18].queen = true;
+            
+            carromMen[9].element.style.backgroundColor = "#01010b";
+            carromMen[10].element.style.backgroundColor = "#01010b";
+            carromMen[11].element.style.backgroundColor = "#01010b";
+            carromMen[12].element.style.backgroundColor = "#01010b";
+            carromMen[13].element.style.backgroundColor = "#01010b";
+            carromMen[14].element.style.backgroundColor = "#01010b";
+            carromMen[15].element.style.backgroundColor = "#01010b";
+            carromMen[16].element.style.backgroundColor = "#01010b";   
+            carromMen[17].element.style.backgroundColor = "#01010b";   
+
     }
 
-    
+
     this.initstriker = function()
     {
     	striker = new CarromPiece();
     	striker.addClass("gotti striker");
     	striker.appendTo(carromBoard);
     	striker.initGottiPos(280,468);
+        striker.element.style.backgroundImage = "url('images/striker.png')";
         striker.radius = 15;
         striker.initSpeed(0,0); 
         striker.angle = 90;  
-        striker.mass = 3;      	        
+        striker.mass = 2;      
         carromMen[totalCarromMen] = striker;
         totalCarromMen++;
         that.initarrow();
@@ -92,20 +138,60 @@ function CarromPieceMove()
         carromBoard.appendChild(arrow);
     }       
 
-
     this.animate = function()
     { 
         window.addEventListener("keydown", carromMen[totalCarromMen-1].eventHandling, false);                               
         function move()
         {            
             for(var i = 0; i < totalCarromMen-1; i++)
-            {''
+            {
                 carromMen[i].detectWall();
-                carromMen[i].moveGotti();                 
+                carromMen[i].moveGotti();   
+
+                var a = carromMen[i].checkhole();
+                if(a == true)
+                {
+                    if(carromMen[i].white == true)
+                    {
+                        totalwhite--;
+                        console.log("white  " + totalwhite);
+                    }
+                    else if(carromMen[i].white = false)
+                    {
+                        totalblack--;
+                        console.log("black  " + totalblack);
+                    }
+
+                    if(carromMen[i].queen == true && totalblack > 1 && totalwhite > 1)
+                    {  
+                        console.log("black  " + totalblack);
+                        console.log("white  " + totalblack);  
+                        carromMen[i].initGottiPos(300, 300);
+                        carromMen[i].initSpeed(0, 0);
+                    }
+
+                    else
+                    {
+                        carromMen[i].element.style.background = "transparent";
+                        carromMen.splice(i, 1);
+                        console.log(carromMen.length);
+                        totalCarromMen = carromMen.length;
+                    } 
+                }
+
+                if(carromMen.length == 1){
+                    console.log("game over");
+                }              
             }
                 //for the striker                                 
                 carromMen[totalCarromMen-1].detectWall();                                          
-                carromMen[totalCarromMen-1].moveGotti();                                    
+                carromMen[totalCarromMen-1].moveGotti();  
+                var b = carromMen[totalCarromMen - 1].checkhole(); 
+                if(b == true)
+                {                    
+                    carromMen[totalCarromMen - 1].initGottiPos(280, 468);
+                    carromMen[totalCarromMen - 1].initSpeed(0, 0);
+                }                                 
             
                 carromMen[totalCarromMen-1].velocity = Math.sqrt(carromMen[totalCarromMen-1].dx*
                 carromMen[totalCarromMen-1].dx + carromMen[totalCarromMen-1].dy*carromMen[totalCarromMen-1].dy);
@@ -115,7 +201,7 @@ function CarromPieceMove()
                 var cen_Y = carromMen[totalCarromMen-1].radius;
 
                 arrow.setAttribute("style", "transform-origin:" + cen_X + 'px' + cen_Y + 'px' ) ; 
-                arrow.style.left = (parseInt(carromMen[totalCarromMen-1].element.style.left) + cen_X) + 'px';
+                arrow.style.left = (parseInt(carromMen[totalCarromMen-1].element.style.left) + cen_X - 1) + 'px';
                 arrow.style.top = (parseInt(carromMen[totalCarromMen-1].element.style.top) + cen_Y) + 'px' ;
                 
                 var a = -(carromMen[totalCarromMen-1].angle);
